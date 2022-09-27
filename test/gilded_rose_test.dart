e88bfd93a499186f0272467587c7c4f564b01cad
Adrian Kujawski <akujawski@mdevelopers.com>
2022-09-27 14:50:25 +0200

    Start using mutation testing

--- test/gilded_rose_test.dart
+++ test/gilded_rose_test.dart
@@ -3,32 +3,7 @@ import 'package:test/test.dart';
 import 'package:gilded_rose/gilded_rose.dart';
 
 main() {
-  final result = """OMGHAI!
--------- day 0 --------
-name, sellIn, quality
-+5 Dexterity Vest, 10, 20
-Aged Brie, 2, 0
-Elixir of the Mongoose, 5, 7
-Sulfuras, Hand of Ragnaros, 0, 80
-Sulfuras, Hand of Ragnaros, -1, 80
-Backstage passes to a TAFKAL80ETC concert, 15, 20
-Backstage passes to a TAFKAL80ETC concert, 10, 49
-Backstage passes to a TAFKAL80ETC concert, 5, 49
-
--------- day 1 --------
-name, sellIn, quality
-+5 Dexterity Vest, 9, 19
-Aged Brie, 1, 1
-Elixir of the Mongoose, 4, 6
-Sulfuras, Hand of Ragnaros, 0, 80
-Sulfuras, Hand of Ragnaros, -1, 80
-Backstage passes to a TAFKAL80ETC concert, 14, 21
-Backstage passes to a TAFKAL80ETC concert, 9, 50
-Backstage passes to a TAFKAL80ETC concert, 4, 50
-
-""";
-
-  test('foo', () {
+  test('report test', () {
     final items = [
       new Item("+5 Dexterity Vest", 10, 20),
       new Item("Aged Brie", 2, 0),
@@ -58,4 +33,67 @@ Backstage passes to a TAFKAL80ETC concert, 4, 50
 
     expect(reporetedText, result);
   });
+
+  void testBackstagePass({int excpected, int sellIn, int quality}) {
+    final item =
+        Item('Backstage passes to a TAFKAL80ETC concert', sellIn, quality);
+    final app = GildedRose([item]);
+    app.updateQuality();
+    expect(item.quality, excpected);
+  }
+
+  void testAgedBrie({int excpected, int sellIn, int quality}) {
+    final item = Item('Aged Brie', sellIn, quality);
+    final app = GildedRose([item]);
+    app.updateQuality();
+    expect(item.quality, excpected);
+  }
+
+  test('Backstage pass increase twice', () {
+    testBackstagePass(excpected: 12, sellIn: 7, quality: 10);
+  });
+
+  test('Backstage pass sellIn less than 5', () {
+    testBackstagePass(excpected: 13, sellIn: 4, quality: 10);
+  });
+
+  test('Backstage pass sellIn zero', () {
+    testBackstagePass(excpected: 0, sellIn: 0, quality: 10);
+  });
+
+  test('Backstage pass sellIn zero', () {
+    testAgedBrie(excpected: 12, sellIn: 0, quality: 10);
+  });
+
+  test('test generic', () {
+    final items = [Item('foo', -1, 3)];
+    final app = GildedRose(items);
+    app.updateQuality();
+    expect(items[0].quality, 1);
+  });
 }
+
+final result = """OMGHAI!
+-------- day 0 --------
+name, sellIn, quality
++5 Dexterity Vest, 10, 20
+Aged Brie, 2, 0
+Elixir of the Mongoose, 5, 7
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 15, 20
+Backstage passes to a TAFKAL80ETC concert, 10, 49
+Backstage passes to a TAFKAL80ETC concert, 5, 49
+
+-------- day 1 --------
+name, sellIn, quality
++5 Dexterity Vest, 9, 19
+Aged Brie, 1, 1
+Elixir of the Mongoose, 4, 6
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 14, 21
+Backstage passes to a TAFKAL80ETC concert, 9, 50
+Backstage passes to a TAFKAL80ETC concert, 4, 50
+
+""";
